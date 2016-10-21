81aec4b563ffe3eb2dd2b8616590a7796eda2568
weiyumou <weiy0007@e.ntu.edu.sg>
2016-10-21 17:34:45 +0800

    updates on api

--- api_annotation.py
+++ api_annotation.py
@@ -1,29 +1,32 @@
 import json
 import re
 
-api_pattern = re.compile("[_a-zA-Z][_a-zA-Z0-9]*\.[_a-zA-Z][_a-zA-Z0-9]*\([_a-zA-z0-9]*\)")
 
-f_api = open("APIs.json", "r+", encoding = "utf-8")
+api_pattern = re.compile("[_a-zA-Z][_a-zA-Z0-9]*\.[_a-zA-Z][_a-zA-Z0-9]*\(")
 
+f_api = open("APIs.json", "r", encoding = "utf-8")
+f_api_annt = open("APIs_Annotated.json", "a", encoding = "utf-8")
+f_api_annt_h = open("APIs_Annotated_HR.json", "a", encoding = "utf-8")
 
-line = json.loads(f_api.readline())
-print(line['Body'])
+count = 0
 
-pos = 0
-match_obj = api_pattern.search(line['Body'], pos)
-while pos < len(line['Body']) and (match_obj is not None):
-    prev = match_obj.string[:match_obj.start()]
-    mid = "<api>" + match_obj.string[match_obj.start():match_obj.end()] + "</api>"
-    last = match_obj.string[match_obj.end():]
-    new_str = prev + mid + last
-    line['Body'] = new_str
-    pos = match_obj.end();
-    match_obj = api_pattern.search(line['Body'], pos)
+for line in f_api:
+    api_dict = json.loads(line)
+    pos = 0
+    match_obj = api_pattern.search(api_dict['Body'], pos)
+    while pos < len(api_dict['Body']) and (match_obj is not None):
+        prev = match_obj.string[:match_obj.start()]
+        mid = "<api>" + match_obj.string[match_obj.start(): match_obj.end() - 1] + "</api>"
+        last = match_obj.string[match_obj.end() - 1: ]
+        new_str = prev + mid + last
+        api_dict['Body'] = new_str
+        count += 1
+        pos = match_obj.end();
+        match_obj = api_pattern.search(api_dict['Body'], pos)
 
-print(line['Body'])
-
-##for line in f_api:
-##    line = json.loads(line)
-    
-
+    f_api_annt.write(json.dumps(api_dict) + "\n")
+    f_api_annt_h.write(json.dumps(api_dict, indent = 4) + "\n")
+        
 f_api.close()
+
+print(count)
--- extract_api.py
+++ extract_api.py
@@ -2,7 +2,7 @@ import re
 import json
 import xml.etree.ElementTree as ET
 
-api_pattern = re.compile("[_a-zA-Z][_a-zA-Z0-9]*\.[_a-zA-Z][_a-zA-Z0-9]*\([_a-zA-z0-9]*\)")
+api_pattern = re.compile("[_a-zA-Z][_a-zA-Z0-9]*\.[_a-zA-Z][_a-zA-Z0-9]*\(")
 
 tree = ET.parse("Posts.xml")
 root = tree.getroot()
@@ -10,14 +10,19 @@ root = tree.getroot()
 f_post = open("Posts.json", "w+", encoding = 'utf-8')
 f_api = open("APIs.json", "w+", encoding = 'utf-8')
 
+count = 0
+
 for child in root:
     json_str = json.dumps(child.attrib, ensure_ascii = False)
     match_obj = api_pattern.search(child.get("Body"))
     if match_obj is not None:
         f_api.write(json_str + "\n")
+        count += 1
     else:
         f_post.write(json_str + "\n")
 
 f_api.close()
 f_post.close()
+
+print(count)
 
