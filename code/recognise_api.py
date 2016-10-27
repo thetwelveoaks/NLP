b0c95b36a5f2c1b00b6368ff82256dfe05cd5cc2
weiyumou <weiy0007@e.ntu.edu.sg>
2016-10-27 13:31:14 +0800

    get all annotated api

--- code/recognise_api.py
+++ code/recognise_api.py
@@ -7,28 +7,16 @@ api_tag = re.compile("<api>" + ".+?" + "</api>")
 api_tree = parsexml("../dataset/APIs_Fully_Annotated.xml")
 api_root = api_tree.getroot()
 api_set = set()
-count = 0
 
-allmatches = api_tag.findall(api_root[0].get("Body"))
-print(allmatches)
+##allmatches = api_tag.findall(api_root[0].get("Body"))
+##print(allmatches)
 ##print(match_obj.string[match_obj.start():match_obj.end()])
 
-##for row in api_root.findall("row"):
-##    body = row.get("Body")
-##    pos = 0
-##    match_obj = api_tag.search(body, pos)
-##    while pos < len(body) and (match_obj is not None):
-####        prev = match_obj.string[:match_obj.start()]
-####        mid = "<api>" + match_obj.string[match_obj.start(): match_obj.end()] + "</api>"
-####        last = match_obj.string[match_obj.end():]
-####        body = prev + mid + last
-##        api_set.add(match_obj.string)
-##        count += 1
-##        pos = match_obj.end();
-##        match_obj = api_tag.search(body, pos)
-
-##    row.set("Body", body)
-
-print(count)
-print(api_set)
+for row in api_root.findall("row"):
+    allmatches = api_tag.findall(row.get("Body"))
+    api_set |= set(allmatches)
+    
+    
+print(len(api_set))
+##print(api_set)
 
